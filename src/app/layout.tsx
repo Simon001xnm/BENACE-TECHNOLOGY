c1ceee0abb1f733ee9a6ab1c2d4269554dda9955
symoh major <simonwanjiru224@gmail.com>
2026-09-14 09:10:15 +0000

    CHANGE THE BRANDING COLOR FROM NAVBLUE TO PAYPAL BLUE

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,7 @@ export const metadata: Metadata = {
 export const viewport: Viewport = {
   width: 'device-width',
   initialScale: 1,
-  themeColor: '#172554',
+  themeColor: '#0070ba',
 };
 
 export default function RootLayout({
